d6bfa8803bc4857c257344a3ce1047ce10802e90
Daniel Kales <11509575+dkales@users.noreply.github.com>
2024-01-23 14:54:49 +0100

    wip on flake for proof-producer

---
+++ flake.nix
@@ -0,0 +1,46 @@
+{
+  description = "C++ Development Shell";
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+  outputs = { self, nixpkgs, flake-utils }:
+    flake-utils.lib.eachDefaultSystem (system:
+
+      let
+        pkgs = import nixpkgs {
+          inherit system;
+        };
+      in
+      with pkgs;
+      {
+        devShells.default = mkShell.override { stdenv = clangStdenv; }
+          {
+            buildInputs = [
+              pkgs.clang-tools
+              pkgs.pkg-config
+              pkgs.cmake
+              pkgs.ninja
+              pkgs.git
+              (pkgs.gnutls.override { cxxBindings = true; })
+              pkgs.openssl
+              pkgs.yaml-cpp
+              pkgs.pkgs.zlib
+              pkgs.zstd
+              pkgs.protobuf3_20
+              pkgs.icu70
+              pkgs.lksctp-tools
+              pkgs.hwloc
+              pkgs.libsystemtap
+              pkgs.libxfs
+              pkgs.ragel
+              pkgs.lksctp-tools
+              pkgs.c-ares
+              pkgs.lz4
+              pkgs.numactl
+              (pkgs.boost180.override { enableShared = false; })
+            ];
+          };
+      }
+    );
+}
